3c2b7cc90b5a627eca181a6883350da98ce868cd
fschwiet <fschwiet@gmail.com>
2018-12-31 14:35:23 -0300

    add MacSplatApp
    This project will let me see what adding nuget packages does for a project targeting xamarin.mac does,
    so I can do the same changes manually in MacSplatconsole

---
+++ MacSplatApp/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace MacSplatApp
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
